d642c0aaeeb10276af2db56bd225ab84a9b9092d
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-12 11:22:14 -0500

    implementacion de alertas en register

--- src/js/register.js
+++ src/js/register.js
@@ -1,6 +1,7 @@
 import { createCompany, findCompany, findCompanyNit } from '../components/async_funtions_auth'
 import '../scss/style.scss'
 import * as bootstrap from 'bootstrap'
+import { smallAlertError } from './alerts'
 
 const formRegister = document.getElementById("form-register")
 const email = document.getElementById("email")
@@ -13,35 +14,53 @@ const imgCompany = document.getElementById("img-company")
 const checkPasswords = () => {
     if (password.value.length < 6) {
         password.classList.add("is-invalid")
+        smallAlertError("password minimo 6 caracteres")
         return false
     }
 
     if (passwordConfirmation.value.length < 6) {
         passwordConfirmation.classList.add("is-invalid")
+        smallAlertError("confirm password minimo 6 caracteres")
         return false
     }
 
     if (password.value === passwordConfirmation.value) {
         return true
     } else {
+        smallAlertError("las contraseñas no coinciden")
         return false
     }
 }
 
 formRegister.addEventListener("submit", async (event) => {
     event.preventDefault()
+    company.classList.add("is-valid")
+    imgCompany.classList.add("is-valid")
+
     const checkEmail = await findCompany(email)
     const checkNit = await findCompanyNit(nit)
     if (checkEmail.data.length != 0 || checkNit.data.length != 0) {
         if (checkEmail.data.length != 0) {
             email.classList.add("is-invalid")
-            alert("este correo ya esta registrado")
+            smallAlertError("ese correo ya esta registrado")
+        } else {
+            email.classList.remove("is-invalid")
+            email.classList.add("is-valid")
         }
+
         if (checkNit.data.length != 0) {
             nit.classList.add("is-invalid")
-            alert("este nit ya esta registrado")
+            smallAlertError("ese nit ya esta registrado")
+        } else {
+            nit.classList.remove("is-invalid")
+            nit.classList.add("is-valid")
         }
+        checkPasswords()
     } else {
+        email.classList.remove("is-invalid")
+        email.classList.add("is-valid")
+        nit.classList.remove("is-invalid")
+        nit.classList.add("is-valid")
         const validatePasswords = checkPasswords()
         if (validatePasswords) {
             const cacheCompany = {
@@ -60,7 +79,6 @@ formRegister.addEventListener("submit", async (event) => {
             }
 
         } else {
-            alert("valida las contraseñas")
             password.classList.add("is-invalid")
             passwordConfirmation.classList.add("is-invalid")
         }
