323b70b04fdd802e61c95c780908f6b1041ad60b
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-12 07:28:26 -0500

    creacion del sistema de login

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,40 @@
 import '../scss/style.scss'
 import * as bootstrap from 'bootstrap'
 
+const formLogin = document.getElementById("form-login")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+
+formLogin.addEventListener("submit", async (event) => {
+    event.preventDefault()
+    const companyFound = await findCompany(email)
+    if (companyFound.data.length === 1) {
+        if (companyFound.data[0].password === password.value) {
+            alert("login exitoso")
+        } else {
+            email.classList.add("is-valid")
+            password.classList.add("is-invalid")
+            alert("password incorrecta")
+        }
+    } else {
+        email.classList.add("is-invalid")
+        alert("esta compañia no esta registrada")
+    }
+})
+
+async function findCompany(email) {
+    const response = await fetch(`http://localhost:3000/companies?email=${email.value}`)
+    if (response.ok) {
+        const data = await response.json()
+        return {
+            ok: response.ok,
+            data: data,
+            statusText: response.statusText
+        }
+    } else {
+        return {
+            ok: response.ok,
+            statusText: response.statusText
+        }
+    }
+}
